0573fe99bee9b722e84cdbf5edbcce577e1c9c93
aaronmeyer <144376981+aaronmeyer@users.noreply.github.com>
2024-01-09 12:37:58 -0500

    Add files via upload

---
+++ chapter12/fitness-bootstrap/fitness-bootstrap/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Aaron Meyer
+    File Name: script.js
+    Date: 1/8/2024
+*/
+
+//jQuery for hero image to consume the header window space
+$ (document).ready(function() {
+    $('.hero').height($(window).height());
+});
