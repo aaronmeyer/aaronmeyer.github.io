1abe630ef8e74229c4ad0d74b069dda776d997f1
aaronmeyer <144376981+aaronmeyer@users.noreply.github.com>
2024-05-07 14:02:36 -0400

    Add files via upload

---
+++ chapter5JavaScript/Projects/Project3/project05-03_txt.js
@@ -0,0 +1,41 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 5
+      Project 05-03
+
+      Project to create a table of headings from an article
+      Author: 
+      Date:   
+
+      Filename: project05-03.js
+*/
+
+let sourceDoc = document.getElementById("source_doc");
+let toc = document.getElementById("toc");
+let headingCount = 1;
+const heading = "H2";
+
+for(let n = sourceDoc.firstElementChild; n !== null; n = n.nextElementSibling){
+    if(n.nodeName === heading){
+        
+        let anchor = document.createElement("a"); 
+
+        anchor.setAttribute("name", "doclink" + headingCount); 
+
+        n.insertBefore(anchor, n.firstChild); 
+
+        let listItem = document.createElement("li"); 
+
+        let link = document.createElement("a"); 
+
+        link.textContent = n.textContent; 
+
+        listItem.appendChild(link);
+
+        link.setAttribute("href", "#doclink" + headingCount);
+
+        toc.appendChild(listItem); 
+
+        headingCount++; 
+    }
+}
